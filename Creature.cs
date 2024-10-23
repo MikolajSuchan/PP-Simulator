c52888c3faa27b313396a3237bb165082d871aa8
Mikołaj Suchan <225887@student.uek.krakow.pl>
2024-10-23 11:50:21 +0200

    Add project files.

---
+++ Creature.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+internal class Creature
+{
+    public string? Name;
+    public int Level;
+
+    public Creature()
+    {
+    }
+
+    public Creature(string? name, int level)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulator\n");
+
+            Creature c = new Creature("x",1);
+            c.SayHi();
+
+        }
+    }
+}
