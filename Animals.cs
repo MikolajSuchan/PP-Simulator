db579b922ac2d27c557745fd537e2d4e4b0ea108
Mikołaj Suchan <225887@student.uek.krakow.pl>
2024-10-26 11:18:25 +0200

    Settery

--- Animals.cs
+++ Animals.cs
@@ -4,9 +4,37 @@ namespace Simulator
 {
     public class Animals
     {
-        public required string Description { get; init; }
+
+        private string _description="Unknown";
+        public string Description
+        {
+            get => _description;
+            init
+            {
+                string trdescription = value.Trim();
+
+                if (trdescription.Length < 3)
+                {
+                    trdescription = trdescription.PadRight(3, '#');
+                }
+                if (trdescription.Length > 15)
+                {
+                    trdescription = trdescription.Substring(0, 15).TrimEnd();
+
+                    if (trdescription.Length < 3)
+                    {
+                        trdescription = trdescription.PadRight(3, '#');
+                    }
+                }
+                if (char.IsLower(trdescription[0]))
+                {
+                    trdescription = char.ToUpper(trdescription[0]) + trdescription.Substring(1);
+                }
+
+                _description = trdescription;
+            }
+        }
         public uint Size { get; set; } = 3;
         public string Info => $"{Description} <{Size}>";
     }
 }
-s
--- Creature.cs
+++ Creature.cs
@@ -2,15 +2,45 @@
 
 internal class Creature
 {
+    private string _name = "Unknown";
+
+    private int _level = 1;
+
     public string Name
     {
-        get;
-        set;
+        get=>_name;
+        init
+        {
+            string trname = value.Trim();
+
+            if (trname.Length < 3)
+            {
+                trname = trname.PadRight(3,'#');
+            }
+            if (trname.Length > 25)
+            {
+                trname = trname.Substring(0, 25).TrimEnd();
+
+                if (trname.Length < 3)
+                {
+                    trname = trname.PadRight(3, '#');
+                }
+            }
+            if (char.IsLower(trname[0]))
+            {
+                trname = char.ToUpper(trname[0]) + trname.Substring(1);
+            }
+
+            _name = trname;
+        }
     }
     public int Level
     {
-        get;
-        set;
+        get=>_level ;
+        init
+        {
+            _level = Math.Max(1, Math.Min(10, value));
+        }
     }
     public Creature()
     {
@@ -24,6 +54,14 @@ internal class Creature
     public void SayHi()
     {
         Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+    }
+
+    public void Upgrade()
+    {
+        if (_level < 10)
+        {
+            _level++;
+        }
     }
 
 }
